1038d97ba0916a927543847132156229dd60ee80
venkateshyekula <74058075+venkateshyekula@users.noreply.github.com>
2022-07-28 14:38:54 +0530

    code changes

--- my-Invoices/src/app/app.module.ts
+++ my-Invoices/src/app/app.module.ts
@@ -1,15 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
